c7e98d7d79a093c582010f6d9719286803e2a9ae
SepsiLaszlo <sepsi.laszlo99@gmail.com>
2021-01-20 17:05:29 +0100

    update config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,6 +21,8 @@ Rails.application.configure do
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.action_cable.disable_request_forgery_protection = true
+
 
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
